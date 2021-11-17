22646b5d3911fae55d9e020c02d3ed02509a043c
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-11-16 23:44:46 -0800

    update

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -4,12 +4,15 @@ module.exports = (error, req, res, next) => {
 
   // if we want to redirect an error to another path
   if (error.type == "redirect") res.redirect("/error");
+
   else if (error.type == "time-out")
     // arbitrary condition check
     res.status(408).send(error);
+
   else if (error?.errno === 1062)// for duplicate errors (mysql code)
     res
       .status(409)
       .send({ success: false, message: error.message });
+      
   else res.status(500).send(error);// all other errors
 };
